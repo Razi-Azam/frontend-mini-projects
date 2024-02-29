af67623a39b4287db260b1669bb7dbf0230d8717
Razi <106505820+Razi-Azam@users.noreply.github.com>
2024-02-29 23:47:50 +0530

    Added a animated tooltip to the subscribe button of modal-popup

--- modal-popup-box/script.js
+++ modal-popup-box/script.js
@@ -1,6 +1,7 @@
 const btnSubs = document.getElementById('btnSubs')
 const btnCancel = document.getElementById('btnCancel')
 const modalContainer = document.querySelector('.modal-container')
+const tooltip = document.querySelector('.tooltip')
 
 btnSubs.addEventListener('click', function () {
     modalContainer.classList.add('popupActive')
@@ -10,3 +11,15 @@ btnCancel.addEventListener('click', function () {
     modalContainer.classList.remove('popupActive')
 
 })
+
+//show/hide tooltip
+btnSubs.addEventListener('mouseover', showToolTip);
+btnSubs.addEventListener('mouseout', hideToolTip);
+
+function showToolTip() {
+    tooltip.style.display = 'block'; 
+}
+
+function hideToolTip() {
+    tooltip.style.display = 'none'; 
+}
